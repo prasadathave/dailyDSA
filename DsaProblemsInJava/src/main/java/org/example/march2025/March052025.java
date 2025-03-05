e7fd2b7faa629719642a5dbaa8d1f67c3cb4c9f1
prasadathave <prasad.athave@iitgn.ac.in>
2025-03-05 06:49:12 +0530

    dailyDSA : march 5th code added

---
+++ DsaProblemsInJava/src/main/java/org/example/march2025/March052025.java
@@ -0,0 +1,9 @@
+package org.example.march2025;
+
+public class March052025 {
+    public long coloredCells(int n) {
+        long k = n;
+        long num= 2*(k-1)*(k-1) + 2*k-1;
+        return num;
+    }
+}
