45a6c341dfbb0aec1f08a9ae813cb524d1a8ac20
prasadathave <prasad.athave@iitgn.ac.in>
2025-03-19 11:19:34 +0530

    dailyDSA : march 19th code added

---
+++ DsaProblemsInJava/src/main/java/org/example/march2025/March19205.java
@@ -0,0 +1,25 @@
+package org.example.march2025;
+
+public class March19205 {
+    public int minOperations(int[] nums) {
+        int op =0;
+
+        for(int i=2; i<nums.length; i++){
+            if(nums[i-2]==0){
+                op++;
+                for(int k =0; k<3; k++){
+                    nums[i-k] = nums[i-k]^1;
+                }
+            }
+        }
+        int sm =0;
+        for(int num:nums){
+            sm+=num;
+        }
+        if(sm!=nums.length){
+            return -1;
+        }
+        return op;
+
+    }
+}
